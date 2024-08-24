a0649e7496066905c8ec45564ec8da7b23abeb7d
Mariah Freire <mahbackesf@gmail.com>
2024-08-24 16:20:13 -0300

    adding player wrapper to get a responsive player

--- app/YouTubePlayer.tsx
+++ app/YouTubePlayer.tsx
@@ -9,11 +9,12 @@ export interface IYouTubePlayerProps {
 
 export const YouTubePlayer = ({playing, videoUrl, setPlayed}:IYouTubePlayerProps) => {
     return (
-        <Stack display={'flex'} position={'absolute'}>
-            <ReactPlayer url={videoUrl} playing={playing} onProgress={(progress) => {
+        /* player wrapper to get a responsive player */
+        <div className='player-wrapper'>
+            <ReactPlayer width={'100%'} height={'100%'} url={videoUrl} playing={playing} onProgress={(progress) => {
             setPlayed(progress.playedSeconds);
             }}/>
-        </Stack>
+        </div>
     );
     
 }
